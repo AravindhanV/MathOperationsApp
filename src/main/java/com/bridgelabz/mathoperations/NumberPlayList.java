ddf17b59e12a623d5a831a495660fb26a5feb24e
Aravindhan V <aravindhan.v@perfios.com>
2021-09-13 19:02:06 +0530

    ADDED: logic to print list using consumer, and implicit and explicit lambda functions

---
+++ src/main/java/com/bridgelabz/mathoperations/NumberPlayList.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.mathoperations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+	public static void main(String[] args) {
+		List<Integer> numberList = new ArrayList<>();
+		for(int i=0;i<5;i++) {
+			numberList.add(i);
+		}
+		
+		//Proper class
+		class MyConsumer implements Consumer<Integer> {
+			public void accept(Integer number) {
+				System.out.println("Proper class: "+number);
+			}
+		}
+		MyConsumer consumer = new MyConsumer();
+		numberList.forEach(consumer);
+		
+		//Anonymous class
+		numberList.forEach(new Consumer<Integer> () {
+			public void accept(Integer number) {
+				System.out.println("Anonymous class: "+number);
+			}
+		});
+		
+		//Explicit Lambda Function
+		Consumer<Integer> explicitConsumer = number -> {
+			System.out.println("Explicit class: "+number);
+		};
+		numberList.forEach(explicitConsumer);
+		
+		//Implicit Lambda Function
+		numberList.forEach(number -> {
+			System.out.println("Implicit class: "+number);
+		});
+	}
+}
