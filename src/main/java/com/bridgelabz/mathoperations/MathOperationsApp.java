d47a322cc24531ffd57bd95d7e56d21331039482
Aravindhan V <aravindhan.v@perfios.com>
2021-09-13 18:39:27 +0530

    REFACTORED: logic for printing result moved to interface

--- src/main/java/com/bridgelabz/mathoperations/MathOperationsApp.java
+++ src/main/java/com/bridgelabz/mathoperations/MathOperationsApp.java
@@ -6,16 +6,19 @@ package com.bridgelabz.mathoperations;
 @FunctionalInterface
 interface IMathFunction {
 	int calculate(int a, int b);
+	static void printResult(String operation, int a, int b, IMathFunction function) {
+		System.out.println(operation+" is "+function.calculate(a, b));
+	}
 }
 
 public class MathOperationsApp {
 	public static void main(String[] args) {
 		IMathFunction add = (a,b) -> a+b;
 		IMathFunction subtract = (a,b) -> a-b;
-		IMathFunction division = (a,b) -> a/b;
-		
-		System.out.println("Addition is "+add.calculate(3, 4));
-		System.out.println("Subtraction is "+subtract.calculate(6, 5));
-		System.out.println("Division is "+division.calculate(18, 3));
+		IMathFunction divide = (a,b) -> a/b;
+
+		IMathFunction.printResult("Addition", 3,4, add);
+		IMathFunction.printResult("Subtraction", 6, 5, subtract);
+		IMathFunction.printResult("Divide",18,3,divide);
 	}
 }
